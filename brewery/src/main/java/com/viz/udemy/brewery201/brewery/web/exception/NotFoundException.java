d1ebbc74d382ce7d753eb53813c58d1da51f40d5
visakh <svisakh@gmail.com>
2022-09-11 11:11:35 +0530

    connect to mapper

---
+++ brewery/src/main/java/com/viz/udemy/brewery201/brewery/web/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.viz.udemy.brewery201.brewery.web.exception;
+
+public class NotFoundException extends RuntimeException{
+
+}
--- brewery/src/main/java/com/viz/udemy/brewery201/brewery/web/service/BeerServiceImpl.java
+++ brewery/src/main/java/com/viz/udemy/brewery201/brewery/web/service/BeerServiceImpl.java
@@ -4,32 +4,56 @@ import java.util.UUID;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+
+import com.viz.udemy.brewery201.brewery.domain.Beer;
+import com.viz.udemy.brewery201.brewery.repostories.BeerRepository;
+import com.viz.udemy.brewery201.brewery.web.exception.NotFoundException;
+import com.viz.udemy.brewery201.brewery.web.mappers.BeerMapper;
 import com.viz.udemy.brewery201.brewery.web.model.BeerDto;
 import com.viz.udemy.brewery201.brewery.web.model.BeerStyleEnum;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class BeerServiceImpl implements BeerService {
+	
+	private final BeerRepository  beerRepository;
+	private final BeerMapper beerMapper;
 
 	
-	public BeerDto getBeerById(UUID beerId) {
+	public BeerDto getBeerById(UUID beerId){
 		// TODO Auto-generated method stub
-		return BeerDto.builder().id(UUID.randomUUID()).beerName("Galaxy Cat")
-				.beerStyle("LAGER_BEER")
-				.build();
+		/*
+		 * return BeerDto.builder().id(UUID.randomUUID()).beerName("Galaxy Cat")
+		 * .beerStyle("LAGER_BEER") .build();
+		 */
+		
+		return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
 	}
 	
 	public BeerDto saveNewBeer(BeerDto beerDto) {
 		
-		return BeerDto.builder().id(UUID.randomUUID()).build();
+		//return BeerDto.builder().id(UUID.randomUUID()).build();
 		
+		return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
 	}
 	
 	public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
-		return BeerDto.builder().id(UUID.randomUUID()).build();
+		//return BeerDto.builder().id(UUID.randomUUID()).build();
+	
+		Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+		beer.setBeerName(beerDto.getBeerName());
+		beer.setBeerStyle(beerDto.getBeerStyle());
+		beer.setPrice(beerDto.getPrice());
+		beer.setUpc(beerDto.getUpc());
+		
+		return beerMapper.beerToBeerDto(beerRepository.save(beer));
 	}
 	
 	public BeerDto deleteBeerById(UUID beerId) {
-		return BeerDto.builder().id(UUID.randomUUID()).build();
+		//return BeerDto.builder().id(UUID.randomUUID()).build();
+		beerMapper.beerToBeerDto(beerRepository.deleteById(beerId));
+		return null;
 	}
-
 }
